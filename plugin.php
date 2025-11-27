039b95b66163d96a1e16fef64ffbd62eb42898a7
Bikendi Tech Solutions Ceo <bikenditechsolutions@outlook.com>
2025-11-27 17:51:20 +0100

    1.0.5

--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
 *Plugin Name: Vtupress Bot
 *Plugin URI: https://bikendi.com
 *Description: This Add-on adds the bot system
-*Version: 1.0.4
+*Version: 1.0.5
 *Author: Akor Victor
 *Author URI: https://facebook.com/bikendi-tech-solutions
 */
@@ -11,3 +11,20 @@ if(!defined('ABSPATH')){
     $pagePath = explode('/wp-content/', dirname(__FILE__));
     include_once(str_replace('wp-content/' , '', $pagePath[0] . '/wp-load.php'));
 };
+
+
+
+require __DIR__.'/plugin-update-checker/plugin-update-checker.php';
+use YahnisElsts\PluginUpdateChecker\v5\PucFactory;
+
+$myUpdateChecker = PucFactory::buildUpdateChecker(
+	'https://github.com/bikendi-tech-solutions/vtupress_woocommerce/',
+	__FILE__,
+	'vtupress_woocommerce'
+);
+//Set the branch that contains the stable release.
+$myUpdateChecker->setBranch('main');
+
+$myUpdateChecker->setAuthentication('your-token-here');
+
+$myUpdateChecker->getVcsApi()->enableReleaseAssets();
